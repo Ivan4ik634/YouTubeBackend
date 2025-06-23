fe18283e9943ffc35951816984e8c96f74dbe95b
Ivan Levadny <ivan@example.com>
2025-06-23 17:17:15 +0300

    fixed import

--- src/statistick/statistick.module.ts
+++ src/statistick/statistick.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DaySchema, MonthSchema, StatistickSchema } from 'src/schemes/Statistick.schema';
+import { Video, VideoSchema } from 'src/schemes/Video.schema';
 import { StatistickController } from './statistick.controller';
 import { StatistickCron } from './statistick.cron';
 import { StatistickService } from './statistick.service';
@@ -11,6 +12,7 @@ import { StatistickService } from './statistick.service';
     MongooseModule.forFeature([{ name: 'Statistick', schema: StatistickSchema }]),
     MongooseModule.forFeature([{ name: 'Day', schema: DaySchema }]),
     MongooseModule.forFeature([{ name: 'Month', schema: MonthSchema }]),
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
   ],
   controllers: [StatistickController],
   providers: [StatistickService, StatistickCron, JwtService],
